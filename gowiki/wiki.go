ce3ff262e078125f5a4f319f2a3308a03a00c547
Ben Leadbetter <leadbetter.ben@outlook.com>
2024-04-03 15:18:14 +0100

    Add handling for viewing non-existent pages

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -38,6 +38,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, err := loadPage(title)
 	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
 	}
 	renderTemplate(w, "view", p)
