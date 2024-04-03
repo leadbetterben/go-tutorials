166114bc1d7653a2205ba8a44ac1bb62d434711d
Ben Leadbetter <leadbetter.ben@outlook.com>
2024-04-03 14:49:30 +0100

    Format wiki go file

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 )
 
 type Page struct {
-    Title string
-    Body  []byte
+	Title string
+	Body  []byte
 }
 
 func (p *Page) save() error {
-    filename := p.Title + ".txt"
-    return os.WriteFile(filename, p.Body, 0600)
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
-    body, err := os.ReadFile(filename)
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
-    return &Page{Title: title, Body: body}, nil
+	return &Page{Title: title, Body: body}, nil
 }
 
 func main() {
-    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-    p1.save()
-    p2, err := loadPage("TestPage")
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, err := loadPage("TestPage")
 	if err != nil {
 		return
 	}
-    fmt.Println(string(p2.Body))
+	fmt.Println(string(p2.Body))
 }
