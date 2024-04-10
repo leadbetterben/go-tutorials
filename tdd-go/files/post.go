8872182e588a03284814fe9053f31ce396ec92cf
Ben Leadbetter <leadbetter.ben@outlook.com>
2024-04-10 16:24:59 +0100

    Add body to blog posts

--- tdd-go/files/post.go
+++ tdd-go/files/post.go
@@ -2,6 +2,8 @@ package blogposts
 
 import (
 	"bufio"
+	"bytes"
+	"fmt"
 	"io"
 	"strings"
 )
@@ -10,6 +12,7 @@ type Post struct {
 	Title       string
 	Description string
 	Tags        []string
+	Body        string
 }
 
 const (
@@ -30,5 +33,15 @@ func newPost(postFile io.Reader) (Post, error) {
 		Title:       readMetaLine(titleSeparator),
 		Description: readMetaLine(descriptionSeparator),
 		Tags:        strings.Split(readMetaLine(tagSeparator), ", "),
+		Body:        readBody(scanner),
 	}, nil
+}
+
+func readBody(scanner *bufio.Scanner) string {
+	scanner.Scan() // ignore a line
+	buf := bytes.Buffer{}
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+	return strings.TrimSuffix(buf.String(), "\n")
 }
